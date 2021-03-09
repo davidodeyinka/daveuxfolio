e889b03aec7aacb0b960025b8f1ca98027e50441
David Odeyinka <david.odeyinka@yahoo.com>
2021-03-09 02:42:38 +0100

    Changed form url back to php script

--- mail/contact.js
+++ mail/contact.js
@@ -15,7 +15,7 @@ $(function () {
             $this.prop("disabled", true);
 
             $.ajax({
-                url: "https://formspree.io/f/xbjpnlpe",
+                url: "conact.php",
                 type: "POST",
                 data: {
                     name: name,
